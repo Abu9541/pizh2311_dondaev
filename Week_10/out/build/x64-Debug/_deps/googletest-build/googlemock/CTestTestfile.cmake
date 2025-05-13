06cf9f091df726a5aa4f1ef49dfff5a00d23819f
Abu Dondaev <dondaevabu126@gmail.com>
2025-05-13 18:29:40 +0300

    Лабораторная работа недели 10. Обновлено.

---
+++ Week_10/out/build/x64-Debug/_deps/googletest-build/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Abu_9541/Desktop/Проекты/Курс 2/4 семестр/ООП/OOP/Week_10/out/build/x64-Debug/_deps/googletest-src/googlemock
+# Build directory: C:/Users/Abu_9541/Desktop/Проекты/Курс 2/4 семестр/ООП/OOP/Week_10/out/build/x64-Debug/_deps/googletest-build/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
---
+++ Week_10/out/build/x64-Debug/tests/number_tests[1]_include.cmake
@@ -0,0 +1,5 @@
+if(EXISTS "C:/Users/Abu_9541/Desktop/Проекты/Курс 2/4 семестр/ООП/OOP/Week_10/out/build/x64-Debug/tests/number_tests[1]_tests.cmake")
+  include("C:/Users/Abu_9541/Desktop/Проекты/Курс 2/4 семестр/ООП/OOP/Week_10/out/build/x64-Debug/tests/number_tests[1]_tests.cmake")
+else()
+  add_test(number_tests_NOT_BUILT number_tests_NOT_BUILT)
+endif()
